3509c355713a0daa84209e3ecc68133b632f75bc
FelipeCasarano <felipe262@outlook.com>
2023-02-16 17:49:16 -0300

    react-ga

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Tracking from "./historyListen"
+
+
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-FHNLGM2NSJ");
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Tracking />
         <Rotas />
       </BrowserRouter>
     </div>
