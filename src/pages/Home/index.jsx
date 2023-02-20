450cd1a8bd9b8987ad73a199078b0ad33e4a5f1a
FelipeCasarano <felipe262@outlook.com>
2023-02-20 15:58:59 -0300

    description

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,6 +13,10 @@ function Home() {
   return (
     <div className="Home">
       <Helmet>
+        <meta
+          name="description"
+          content="A E.C.Money é uma empresa de Assessoria Financeira & Tecnologia Empresarial que ajuda empresas a obter resultados surpreendentes superando a concorrência, utilizando a criatividade, tecnologia e direcionando seus recursos de forma eficiente."
+        />
         <link rel="canonical" href="/metodologia" />
       </Helmet>
       <NavBar />
