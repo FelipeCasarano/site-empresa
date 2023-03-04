96748a30c214dab4a2e3ee4c99325e353a2604fd
FelipeCasarano <felipe262@outlook.com>
2023-03-04 20:04:43 -0300

    gtm

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tracking from "./historyListen"
-
-
-import ReactGA from "react-ga4";
+import ReactGA from "react-ga4"
+import TagManager from 'react-gtm-module'
+const tagManagerArgs = {
+  gtmId: 'GTM-KXCJGQ2'
+}
+TagManager.initialize(tagManagerArgs)
 
 ReactGA.initialize("G-FHNLGM2NSJ");
+
 
 function App() {
   return (
